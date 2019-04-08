1518055ec8c5339ab39f76fb0143350ae3c63381
minlucyzhao <minlucyzhao@gmail.com>
2019-04-08 17:18:45 +0000

    Prints avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -1,15 +1,19 @@
-var request = require('request');
-var secret = require('secrets');
+var request = require('request'); //access external module
+var secrets = require('./secrets'); // "./" to access module files within the folder
 
     function getRepoContributors(repoOwner, repoName, cb) {
         var options = {
-            url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+            url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
             headers: {
-            'User-Agent': 'request'
+            'User-Agent': 'request',
+            Authorization: 'token ' + secrets.GITHUB_TOKEN //Authorization: token YOUR_TOKEN_HERE --> in this format
           }
         };
         request(options, function(err, res, body) {
-          cb(err, body);
+          //parse the JSON string into an object
+          var obj = JSON.parse(body)
+          //passes the object to the callback function
+          cb(err, obj);
         });
     }
 
@@ -17,5 +21,9 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 getRepoContributors("jquery", "jquery", function(err, result) {
     console.log("Errors:", err);
-    console.log("Result:", result);
+    //iterate over the results and console log the value of each avatar_url 
+    result.forEach(function(result) {
+        var avatarURL = result.avatar_url;
+        console.log("Avatar URL:", avatarURL);
+    });
   });
